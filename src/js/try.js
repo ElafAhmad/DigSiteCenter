63ec9c304c4055994dbcee19565df6374f6c6aec
ElafAhmad <elafalamri7@gmail.com>
2019-07-22 11:38:11 +0300

    Linked Add application function
    Now? Check if it's work on Ubinto! Hope so!!!

--- src/js/try.js
+++ src/js/try.js
@@ -5,48 +5,53 @@ App = {
     init: async function() {
       // Load pets.
       App.init();
-      $.getJSON('../applenew.json', function(data) {
-        var petsRow = $('#petsRow');
-        var petTemplate = $('#petTemplate');
+      // $.getJSON('../applenew.json', function(data) {
+      //   var petsRow = $('#petsRow');
+      //   var petTemplate = $('#petTemplate');
   
-        for (i = 0; i < data.length; i ++) {
-          petTemplate.find('.panel-title').text(data[i].name);
-          petTemplate.find('img').attr('src', data[i].cost);
-          petTemplate.find('.pet-breed').text(data[i].depth);
-          petTemplate.find('.pet-age').text(data[i].length);
-          petTemplate.find('.pet-location').text(data[i].location);
-          petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+      //   for (i = 0; i < data.length; i ++) {
+      //     petTemplate.find('.panel-title').text(data[i].name);
+      //     petTemplate.find('img').attr('src', data[i].cost);
+      //     petTemplate.find('.pet-breed').text(data[i].depth);
+      //     petTemplate.find('.pet-age').text(data[i].length);
+      //     petTemplate.find('.pet-location').text(data[i].location);
+      //     petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
   
-          petsRow.append(petTemplate.html());
-        }
-      });
+      //     petsRow.append(petTemplate.html());
+      //   }
+      // });
   
-      return await App.initWeb3();
+      return App.initWeb3();
     },
   
     initWeb3: async function() {//START OF META MASK CHECKS
       // Modern dapp browsers...
-  if (window.ethereum) {
-    App.web3Provider = window.ethereum;
-    try {
-      // Request account access
-      await window.ethereum.enable();
-    } catch (error) {
-      // User denied account access...
-      console.error("User denied account access")
-    }
+  // if (window.ethereum) {
+  //   App.web3Provider = window.ethereum;
+  //   try {
+  //     // Request account access
+  //     await window.ethereum.enable();
+  //   } catch (error) {
+  //     // User denied account access...
+  //     console.error("User denied account access")
+  //   }
+  // }
+  // // Legacy dapp browsers...
+  // else if (window.web3) {
+  //   App.web3Provider = window.web3.currentProvider;
+  // }
+  // // If no injected web3 instance is detected, fall back to Ganache
+  // else {
+  //   App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+  // }
+  // web3 = new Web3(App.web3Provider);
+  if (typeof web3 !== 'undefined'){
+    App.web3Provider = web3.currentProvider;
+  } else {
+    App.web3Provider = new Web3.providers.HttpProvider('HTTP://127.0.0.1:7545');
   }
-  // Legacy dapp browsers...
-  else if (window.web3) {
-    App.web3Provider = window.web3.currentProvider;
-  }
-  // If no injected web3 instance is detected, fall back to Ganache
-  else {
-    App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-  }
-  web3 = new Web3(App.web3Provider);
-  
-  
+    web3 = new Web3(App.web3Provider);
+
       return App.initContract();
     },//END OF META MASK CHECKS
   
@@ -59,15 +64,20 @@ App = {
         // Set the provider for our contract
         App.contracts.Apple.setProvider(App.web3Provider);
       
-        // Use our contract to retrieve and mark the adopted pets
-        // return App.markAdopted();
-      });
-  
+           web3.eth.getAccounts(function(error, accounts) {
+        if (error) {
+          console.log(error);
+        }
+      
+        var account = accounts[0];
+           });
+          });
+         
       return App.bindEvents();
     },
   
     bindEvents: function() {
-      $(document).on('click', '.btn-adopt', App.handleAdopt);
+      $(document).on('click', '#buttonS', App.handleApply);
     },
   
 //     markAdopted: function(adopters, account) {
@@ -89,27 +99,44 @@ App = {
   
 //     },
   
-    handleAdopt: function(event) {
+    handleApply: function(event) {
       event.preventDefault();
-  
-      var ApplicationId = parseInt($(event.target).data('id'));
+      
+      var Aname = $('#name').val();
+      var Alocation = $('#location').val();
+      var Alength = $('#length').val();
+      var Adepth = $('#depth').val();
+      var AsDate = $('#sDate').val();
+      var AeDate = $('#eDate').val();
+      var cost = $('#cost').val();
+      console.log(Aname+"\n"+Alocation+"\n"+Alength+"\n"+Adepth+"\n"+AsDate+"\n"+AeDate+"\n"+cost+"\n");
+
+      //var ApplicationId = parseInt($(event.target).data('id'));
       var applicationInstance;
+      App.contracts.Apple.deployed().then(function(instance) {
+        applicationInstance = instance;
+        return applicationInstance.application(Aname.toString(), Alocation.toString(), Adepth, Alength, cost, AsDate.toString(), AeDate.toString());
+      }).then(function(result){
+        alert('Application was added!');
+            }).catch(function(err){
+              console.log(err.message);
+            })
   
-      web3.eth.getAccounts(function(error, accounts) {
-        if (error) {
-          console.log(error);
-        }
+      // web3.eth.getAccounts(function(error, accounts) {
+      //   if (error) {
+      //     console.log(error);
+      //   }
       
-        var account = accounts[0];
+      //   var account = accounts[0];
       
-        App.contracts.Apple.deployed().then(function(instance) {
-          applicationInstance = instance;
+      //   App.contracts.Apple.deployed().then(function(instance) {
+      //     applicationInstance = instance;
       
-          // Execute adopt as a transaction by sending account
-          return applicationInstance.setstatues(ApplicationId, {from: account});
+      //     // Execute adopt as a transaction by sending account
+      //     return applicationInstance.setstatues(ApplicationId, {from: account});
         
-        });
-      });
+      //   });
+      // });
   
       
     }
